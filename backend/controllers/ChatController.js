601616eca785de541f88e6f988e41d888ead4df4
Gaston <gaston.quittner@gmail.com>
2023-01-14 21:58:29 -0600

    1:N Create Group Chat

--- backend/controllers/ChatController.js
+++ backend/controllers/ChatController.js
@@ -35,7 +35,7 @@ export const accessChat = asyncHandler(async (req, res, next) => {
 
         try {
             const createdChat = await Chat.create(chatData);
-            const fullChat = await Chat.findOne({ _id: createdChat._id }).populate("users", "-password");
+            const fullChat = await Chat.findOne({ _id: createdChat._id }).populate("users", "id name email");
 
             res.status(200).send(fullChat);
         } catch (error) {
@@ -45,9 +45,64 @@ export const accessChat = asyncHandler(async (req, res, next) => {
     }
 });
 
-export const fetchChats = asyncHandler(async (req, res, next) => {});
+export const fetchChats = asyncHandler(async (req, res, next) => {
+    try {
+        const chats = await Chat.find({
+            users: { $elemMatch: { $eq: req.user._id } },
+        })
+            .populate("users", "id name email")
+            .populate("groupAdmin", "-password")
+            .populate("latestMessage")
+            .sort({ updatedAt: -1 })
+            .then(async (results) => {
+                results = await User.populate(results, {
+                    path: "latestMessage.sender",
+                    select: "name photo email",
+                });
 
-export const createGroupChat = asyncHandler(async (req, res, next) => {});
+                res.status(200).send(results);
+            });
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+
+export const createGroupChat = asyncHandler(async (req, res, next) => {
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please fill all the fields." });
+    }
+
+    try {
+        var users = JSON.parse(req.body.users);
+        if (users.length < 2) {
+            return res.status(400).json({ message: "More than 2 users are required to form a group chat." }).send();
+        }
+
+        users.push(req.user); // After all users are created, the creator (current logged in user) is added too.
+
+        try {
+            const groupChat = await Chat.create({
+                chatName: req.body.name,
+                users: users,
+                isGroupChat: true,
+                groupAdmin: req.user,
+            });
+
+            const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+                .populate("users", "id name email")
+                .populate("groupAdmin", "id name email");
+
+            res.status(200).json(fullGroupChat);
+        } catch (error) {
+            res.status(400);
+            throw new Error(error.message);
+        }
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
 
 export const renameGroupChat = asyncHandler(async (req, res, next) => {});
 
